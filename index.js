e53f403e6158b350fb08a315e126045863994311
Ryan Bethel <ryan.bethel@gmail.com>
2024-06-01 06:16:04 -0400

    export paths

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import elements from "../elements.js";
-import components from "../components.js";
+import elements from "./elements.js";
+import components from "./components.js";
 export { elements, components }
